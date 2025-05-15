2db0fae2bfbf6f2605d6e8efad1375730f22a551
DanielZenkin476 <Danielzenkin82@gmail.com>
2025-05-15 15:29:50 +0300

    made comment changes and small cleanup, added gitignore from main branch

--- backend.py
+++ backend.py
@@ -2,39 +2,46 @@ from flask import Flask, redirect, render_template, request
 from datetime import timedelta, datetime
 import os 
 
+# The service exposes three APIs:
+# `GET /<room>` Will return the static HTML (the provided front end), regardless of the room provided.
+# `POST /api/chat/<room>`Accepts two form data fields - `username` and `message`.Will save date, time, username and message per room.
+# `GET /api/chat/<room>` Returns the full chat in a room: Chat formatted as a list of `\n` delimited lines (1 per message)Each line formatted according to the example:
+#"[2024-09-10 14:00:51] Roey: Hi everybody!"
+#made by Yuval Dar & Daniel Zenkin
+ 
 app = Flask(__name__)
-app.secret_key = "hello"
-app.permanent_session_lifetime = timedelta(days=1)
+app.permanent_session_lifetime = timedelta(days=1) # define app ttl
 CHAT_FOLDER = 'chats'  # Directory to store chat files
-os.makedirs(CHAT_FOLDER, exist_ok=True)
+os.makedirs(CHAT_FOLDER, exist_ok=True) #make chats folder if not there
 # dictionary to store chat
 
-# implemented by Daniel
+# 1. A implemented by Daniel
 @app.route("/")
 def home():
 	return render_template("index.html")
 
-# implemented by Yuvalb
+# 1. B implemented by Yuval
 @app.route('/<room>', methods=['GET'])
 def get_chat(room):
     # return chat for the room or an empty string if no messages exist
-    # return "\n".join(chats.get(room, [])) # fail option
 	return render_template("index.html")
-	
+
+#2. A implemented by Daniel
 @app.route('/api/chat/<room>', methods=['GET'])
 def get_chat_history(room):
     #Returns the full chat in a room, newline delimited
     filename = os.path.join(CHAT_FOLDER, f'{room}.txt')
-    try:
+    try: # try to read file, if not exists will return empty
         with open(filename, 'r') as f:
             chat_history = f.read()
         return chat_history, 200, {'Content-Type': 'text/plain'}
     except FileNotFoundError:
         return "", 200, {'Content-Type': 'text/plain'}
     
-
+# 2. B implemented by Yuval
 @app.route('/api/chat/<room>', methods=['POST'])
 def post_chat(room):
+    #function to add message and write it to a text file for storage
     username = request.form.get('username')
     msg = request.form.get('msg')
 
@@ -54,6 +61,6 @@ def post_chat(room):
 
     return "success", 200  # Returning plain string
 
-
+### MAIN #####
 if __name__ == "__main__":
 	app.run(debug=True)
