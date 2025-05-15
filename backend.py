eeea9d4a4afe468f1227bf348d0a744f73427ff3
Yuval <yuvaldar@gmail.com>
2025-05-15 12:39:47 +0300

    first branch commit

--- backend.py
+++ backend.py
@@ -1,13 +1,22 @@
 from flask import Flask, redirect, render_template, request
+from datetime import timedelta
+
 app = Flask(__name__)
 app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(days=1)
+
+# Dictionary to store chat
+chats = {}
 
 @app.route("/")
 def home():
 	return render_template("index.html")
 
 
+@app.route('/api/chat/<room>', methods=['GET'])
+def get_chat(room):
+    # Return chat for the room or an empty string if no messages exist
+    return "\n".join(chats.get(room, []))
 
 
 
